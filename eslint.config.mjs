786c4e9601eb27e21925300ca71463b36cc25b81
Joshua Eze <jezeadiebuo5@gmail.com>
2025-04-02 08:18:00 +0100

    updated eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "no-explicit-any": "off", // Disable the no-explicit-any rule
+      "@typescript-eslint/no-explicit-any": "off" // Disable the no-explicit-any rule
     },
   },
 ];
