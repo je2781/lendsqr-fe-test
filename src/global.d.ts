377bd4d9131c3807c7ef8b5b3af93ba32a643964
Joshua Eze <jezeadiebuo5@gmail.com>
2025-04-04 17:06:30 +0100

    updated global type window with local storage property for identification

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,9 +1,5 @@
-export interface Window {
-    localeStorage: {
-      getItem: (key: string) => void,
-      setItem: (key: string, value: string) => void,
-      remove: (key: string) => void
-    }
+interface Window {
+    localeStorage: Storage
   }
   
   
