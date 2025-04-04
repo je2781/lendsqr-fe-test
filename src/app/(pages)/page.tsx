95afb65c5d1560e994f1ca70cb683b1e512cf3ac
Joshua Eze <jezeadiebuo5@gmail.com>
2025-04-04 20:42:35 +0100

    still get windows not defined during dev. probably because the main layout is serverside rendered

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -2,36 +2,24 @@
 
 import React from "react";
 import { useRouter } from "next/navigation";
-import Dashboard from "@/components/dashboard/Dashboard";
 
 export default function Home() {
   const router = useRouter();
 
-  let userDataIsAvailable = false;
-
-  if (typeof window !== "undefined") {
-    userDataIsAvailable = !!window.localStorage.getItem("users");
-  }
+  const redirecting = (
+    <div className="min-h-screen w-full flex flex-col justify-center items-center">
+      <div className="flex flex-col items-center">
+        {/* Spinner */}
+        <div className="animate-spin h-8 w-8 border-4 border-secondary-400 border-t-transparent rounded-full"></div>
+        {/* Text */}
+        <p className="mt-3 text-lg text-primary-400">Redirecting...</p>
+      </div>
+    </div>
+  );
 
   React.useEffect(() => {
     router.replace("/login");
   }, []);
 
-  // Prevent rendering until authentication is confirmed
-  if (!userDataIsAvailable){
-    return (
-      <div className="min-h-screen w-full flex flex-col justify-center items-center">
-        <div className="flex flex-col items-center">
-          {/* Spinner */}
-          <div className="animate-spin h-8 w-8 border-4 border-secondary-400 border-t-transparent rounded-full"></div>
-          {/* Text */}
-          <p className="mt-3 text-lg text-primary-400">Redirecting...</p>
-        </div>
-      </div>
-    );
-  }else{
-    return null;
-  }
-    
-
+  return redirecting;
 }
